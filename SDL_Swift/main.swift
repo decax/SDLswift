072b1d486004d949dd37a14ce0306b60147c9692
Dominic Hudon <dominic.hudon@gmail.com>
2018-02-18 10:59:34 -0500

    Initial Commit

---
+++ SDL_Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SDL_Swift
+//
+//  Created by Dominic Hudon on 18-02-18.
+//  Copyright © 2018 Dominic Hudon. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
