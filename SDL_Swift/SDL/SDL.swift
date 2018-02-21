406fef5548437c06f0a5a3306a30e6d3a01a91d0
Dominic Hudon <dominic.hudon@gmail.com>
2018-02-20 21:54:37 -0500

    Shit, lots of stuff

---
+++ SDL_Swift/SDL/SDL.swift
@@ -0,0 +1,86 @@
+//
+//  SDL.swift
+//  SDL_Swift
+//
+//  Created by Dominic Hudon on 18-02-03.
+//  Copyright © 2018 Dominic Hudon. All rights reserved.
+//
+
+import Foundation
+
+struct SDL {
+
+    struct System: OptionSet {
+        let rawValue: Int
+        
+        static let timer          = System(rawValue: 0x01)
+        static let audio          = System(rawValue: 0x10)
+        static let video          = System(rawValue: 0x20)
+        static let joystick       = System(rawValue: 0x200)
+        static let haptic         = System(rawValue: 0x1000)
+        static let gameController = System(rawValue: 0x2000)
+        static let events         = System(rawValue: 0x4000)
+        static let noParachute    = System(rawValue: 0x100000)
+
+        static let Everything: System = [.timer, .audio, .video, .joystick, .haptic, .gameController, .events]
+    }
+
+    static func Init(system: System) {
+        SDL_Init(CUnsignedInt(system.rawValue))
+    }
+
+    static func Quit() {
+        SDL_Quit()
+    }
+    
+    static func ShowSimpleMessageBox(title: String, message: String) {
+        SDL_ShowSimpleMessageBox(0, title, message, nil);
+    }
+}
+
+struct Point {
+    var x: Int
+    var y: Int
+}
+
+struct Size {
+    var width: Int
+    var height: Int
+}
+
+struct Rectangle {
+    var x: Int
+    var y: Int
+    var width: Int
+    var height: Int
+    
+    static func +=(left: inout Rectangle, offset: Point) {
+        
+        left.x += offset.x
+        left.y += offset.y
+    }
+}
+
+struct Color {
+    var red: UInt8
+    var green: UInt8
+    var blue: UInt8
+    var alpha: UInt8
+    
+    init(red: UInt8, green: UInt8, blue: UInt8, alpha: UInt8) {
+        self.red = red
+        self.green = green
+        self.blue = blue
+        self.alpha = alpha
+    }
+    
+    init(red: UInt8, green: UInt8, blue: UInt8) {
+        self.init(red: red, green: green, blue: blue, alpha: 255)
+    }
+    
+    static var white = Color(red: 255, green: 255, blue: 255)
+    static var black = Color(red: 0, green: 0, blue: 0)
+    static var red_ = Color(red: 255, green: 0, blue: 0)
+    static var green_ = Color(red: 0, green: 255, blue: 0)
+    static var blue_ = Color(red: 0, green: 0, blue: 255)
+}
--- SDL_Swift/main.swift
+++ SDL_Swift/main.swift
@@ -1,12 +1,98 @@
-//
-//  main.swift
-//  SDL_Swift
-//
-//  Created by Dominic Hudon on 18-02-18.
-//  Copyright © 2018 Dominic Hudon. All rights reserved.
-//
-
 import Foundation
 
-print("Hello, World!")
+print("SDL Version: \(SDL.GetVersion())")
 
+SDL.Init(system: [.video, .audio, .events])
+SDL_Image.Init()
+
+for driver in SDL.Renderer.GetDriversInfos() {
+    print("\(driver.name) min: \(driver.maxTextureWidth) max: \(driver.maxTextureHeight)")
+}
+
+var window = SDL.Window(title: "Hello World!", x: 100, y: 100, width: 640, height: 480);
+var renderer = SDL.Renderer(window: window, flags: .software)
+
+renderer.drawColor = Color.black
+let _ = renderer.drawColor
+
+let surface = SDL.Surface(width: 50, height: 50, pixelFormat: .rgba8888)
+//surface.FillRect(rectangle: Rectangle(x: 1, y: 1, width: 10, height: 10), color: Color(red: 255, green: 0, blue: 0))
+surface.Fill(color: Color(red: 255, green: 0, blue: 0))
+
+let surface2 = SDL.Surface(filename: "Axel.png")!
+
+//let texture = Texture(renderer: renderer, fromSurface: surface)
+let texture2 = SDL.Texture(renderer: renderer, fromSurface: surface2)
+let texture3 = SDL.Texture(renderer: renderer, filename: "Axel.png")!
+
+var offset = 150
+
+var running = true
+
+while running {
+
+    if let event = SDL.PollEvent() {
+        
+        switch event.id {
+        
+        case .quit:
+            running = false
+        
+        case .keyDown:
+            print("keydown")
+            if let keyboardEvent = event as? SDL.KeyboardEvent {
+                switch keyboardEvent.keySym.scancode {
+                case .escape:
+                    running = false
+
+                case .enter:
+                    SDL.ShowSimpleMessageBox(title: "patate", message: "salut patate")
+
+                default:
+                    offset /= 2
+                }
+                
+                if keyboardEvent.keySym.mod.contains(.leftShift) && keyboardEvent.keySym.mod.contains(.rightShift) {
+                    print("LEFT & RIGHT SHIIIIIIFT")
+                }
+            }
+        
+        case .keyUp:
+            print("keyup")
+            
+        default:
+            ()
+        }
+    }
+    
+    renderer.clear()
+    
+//    renderer.copy(texture: texture, position: Point(x: 50, y: 50))
+    renderer.copy(texture: texture2, destinationRect: Rectangle(x: 200, y: 0, width: Int(texture2.width / 4), height: Int(texture2.height / 4)))
+    
+    renderer.drawBlendMode = .none
+    texture3.alphaMod = 85
+    
+    var rect = Rectangle(x: 0, y: 200, width: Int(texture3.width / 4), height: Int(texture3.height / 4))
+    
+    rect.x = 200 - offset
+    texture3.colorMod = Color.red_
+    renderer.copy(texture: texture3, destinationRect: rect)
+    
+    rect.x = 200
+    texture3.colorMod = Color.green_
+    renderer.copy(texture: texture3, destinationRect: rect)
+    
+    rect.x = 200 + offset
+    texture3.colorMod = Color.blue_
+    renderer.copy(texture: texture3, destinationRect: rect)
+    
+    renderer.present()
+}
+
+//print("\(SDL.GetTicks())")
+//SDL.Delay(ms: 1000)
+//print("\(SDL.GetTicks())")
+
+SDL_Image.Quit()
+SDL.Quit()
